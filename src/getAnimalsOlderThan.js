2bea29f8b75a1be3acd8cfcd36aa89e88cd3233a
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-02 20:19:39 -0400

    Req2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((animalName) => animalName.name === animal)
+    .residents.every((element) => element.age >= age);
 }
 
+console.log(getAnimalsOlderThan('lions', 10))
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids){
+  return data.species.filter((id) => ids.includes(id))
 }
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
