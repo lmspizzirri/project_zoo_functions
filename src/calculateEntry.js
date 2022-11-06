14bc2ccafc3f6834c82f54fa7d9855dfdc5d4a5d
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-06 09:42:40 -0500

    Req 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,30 @@
 const data = require('../data/zoo_data');
+const entrants = [
+	{ name:  'Lara Carvalho', age:  5 },
+	{ name:  'Frederico Moreira', age:  5 },
+	{ name:  'Pedro Henrique Carvalho', age:  5 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+];
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(entrants) {  
+  let visitors = {};  
+  visitors.child = entrants.filter((check) => check.age < 18).length;
+  visitors.adult = entrants.filter((check) => check.age >= 18 && check.age < 50).length;
+  visitors.senior = entrants.filter((check) => check.age >= 50).length;
+  return visitors;
 }
-
+console.log(countEntrants(entrants));
 function calculateEntry(entrants) {
-  // seu código aqui
+  if(entrants === undefined){
+    return 0;
+  }
+  if(Object.keys(entrants).length === 0){
+    return 0;
+  }
+  const visitors = countEntrants(entrants);
+  let value = visitors.child * 20.99 + visitors.adult * 49.99 + visitors.senior * 24.99;
+  return value;
 }
-
-
 module.exports = { calculateEntry, countEntrants };
