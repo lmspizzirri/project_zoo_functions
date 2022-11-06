af460708b3128518e766130eca43c1fa0a2dcf57
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-06 06:37:20 -0500

    Req 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined){
+    const allAnimals = {};
+    data.species.forEach((especie) => allAnimals[especie.name] = especie.residents.length);
+    return allAnimals;
+  } else if (animal.sex === undefined){
+    return data.species.find((especie) => especie.name === animal.specie).residents.length;
+  } else{
+    return data.species.find((especie) => especie.name === animal.specie).residents.filter((especie) => especie.sex === animal.sex).length;
+  }
 }
 
+console.log(countAnimals({ specie: 'giraffes'}))
 module.exports = countAnimals;
