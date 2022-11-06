580fb3fec0c74e5ccbaff07781e10f0e16c041b9
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-06 10:58:39 -0500

    Req 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -5,5 +5,9 @@ describe('Testes da função getOpeningHours', () => {
         expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
         expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
         expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+        expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');  
+        expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+        expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+        expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
     });
 });
