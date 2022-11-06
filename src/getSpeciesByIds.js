39f46ede69496c3b6bdd5eaaa3cb1893666fd5e9
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-06 07:03:19 -0500

    Req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids){
-  return data.species.filter((id) => ids.includes(id))
+  return data.species.filter(({id}) => ids.includes(id));
 }
 
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
