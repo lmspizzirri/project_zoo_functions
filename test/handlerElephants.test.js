adbc013521f193bf13b9b5e27016acd35890c11a
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-06 07:48:27 -0500

    Req 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,10 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    it('Verifica se após receber os parâmetros temos os resultados a seguir', () =>{
+        expect(handlerElephants()).toBe(undefined);
+        expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+        expect(handlerElephants('popularity')).toEqual(5);
+        expect(handlerElephants('count')).toBe(4);
+    });
+});    
