be3195dad14915704a0db21c5d4885fc13344c4c
lmspizzirri <lm.spizzirri@gmail.com>
2022-11-10 04:20:36 -0500

    Req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalIdentify = data.employees.find((ppl) => ppl.id === id).responsibleFor[0];
+  const animal = data.species.find((animalId) => animalId.id === animalIdentify)
+  const oldestAnimal = animal.residents.reduce((acc, currentValue) => (acc.age > currentValue.age ? acc : currentValue));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
-
+getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992');
 module.exports = getOldestFromFirstSpecies;
